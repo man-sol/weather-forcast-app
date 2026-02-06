641d98e4eaf3df1b06f2c0cf47f6305c1d122c7e
Manish kumar <manishnkb7@gmail.com>
2026-02-07 00:43:14 +0530

    adding script.js flie, writing api ley and targeting DOM elements

---
+++ script.js
@@ -0,0 +1,26 @@
+// CONFIG
+const API_KEY = "4e0f2a991f315f88bb1699b720d1250a";
+
+
+//   DOM ELEMENTS
+
+const cityInput = document.getElementById("cityInput");
+const searchBtn = document.getElementById("searchBtn");
+const locationBtn = document.getElementById("locationBtn");
+const toggleTempBtn = document.getElementById("toggleTemp");
+const recentCities = document.getElementById("recentCities");
+const errorMsg = document.getElementById("errorMsg");
+
+const cityName = document.getElementById("cityName");
+const temperatureEl = document.getElementById("temperature");
+const description = document.getElementById("description");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+const weatherIcon = document.getElementById("weatherIcon");
+const forecast = document.getElementById("forecast");
+const alertBox = document.getElementById("alertBox");
+
+const weatherSection = document.getElementById("weatherSection");
+const emptyState = document.getElementById("emptyState");
+
+
